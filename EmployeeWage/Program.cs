f752e28985b4b487a34b57f7365296fcd246ae68
unknown <asthaagarwal203@gmail.com>
2022-02-14 21:20:52 +0530

    [Astha] Add:EmployeeWageUC3

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -10,12 +10,13 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to employee wage computation program");
-                Random random=new Random();
-            if (random.Next(0, 2) == 0)
-                Console.WriteLine("Employee is Absent");
-            else
-                Console.WriteLine("Employee is Present");
+            int WAGE_PER_HOUR = 20;
+            int FULL_DAY_HOUR = 8;
+            int PART_TIME_HOUR = 8;
+            int dailyEmployeeWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+            int partTimeEmployeeWage = PART_TIME_HOUR * WAGE_PER_HOUR;
+            Console.WriteLine("Daily employee wage is "+dailyEmployeeWage);
+            Console.WriteLine("Part time employee wage is " + partTimeEmployeeWage);
         }
     }
 }
