fa35b8a6803cd36fa0c5e4134b6f01e36295d6fe
unknown <asthaagarwal203@gmail.com>
2022-02-14 12:08:53 +0530

    [Astha] Add: EmployeeWAgeUC1

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee wage computation program");
+        }
+    }
+}
