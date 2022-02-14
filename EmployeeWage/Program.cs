fff9ad63f3310d93c616b70724d9e10b5bd890ff
unknown <asthaagarwal203@gmail.com>
2022-02-14 22:10:05 +0530

    [Astha] Add: EmployeeWageUC6

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -19,18 +19,21 @@ namespace EmployeeWage
             int day = 0;
             Random random = new Random();
             int totalWage = 0;
-            while (day < 20)
+            int totalWorkingHours = 0;
+            while (day < 20&&totalWorkingHours<100)
             {
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
                 {
                     case PARTIAL_PRESENT:
                         totalWage += PART_TIME_HOUR * WAGE_PER_HOUR;
+                        totalWorkingHours += PART_TIME_HOUR;
                         break;
                     case ABSENT:
                         break;
                     case PRESENT:
                         totalWage += WAGE_PER_HOUR * FULL_DAY_HOUR;
+                        totalWorkingHours += FULL_DAY_HOUR;
                         break;
                 }
                 day++;
