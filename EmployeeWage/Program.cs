d5b78445b66c4a4253967666a1a951bc20218c64
unknown <asthaagarwal203@gmail.com>
2022-02-14 12:14:21 +0530

    [Astha] Add: EmployeeWageUC2

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -11,6 +11,11 @@ namespace EmployeeWage
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to employee wage computation program");
+                Random random=new Random();
+            if (random.Next(0, 2) == 0)
+                Console.WriteLine("Employee is Absent");
+            else
+                Console.WriteLine("Employee is Present");
         }
     }
 }
