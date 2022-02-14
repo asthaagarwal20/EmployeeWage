ae31f4e52ea851c9406c4058ba0d9fd78674ec8b
asthaagarwal20 <81606308+asthaagarwal20@users.noreply.github.com>
2022-02-14 12:18:04 +0530

    Update Program.cs

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +11,11 @@ namespace EmployeeWage
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to employee wage computation program");
+                Random random=new Random();
+            if (random.Next(0, 2) == 0)
+                Console.WriteLine("Employee is Absent");
+            else
+                Console.WriteLine("Employee is Present");
         }
     }
 }
