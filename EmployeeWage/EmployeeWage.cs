1919c692887329e306aa3377ce241c089a419333
unknown <asthaagarwal203@gmail.com>
2022-02-23 19:20:58 +0530

    [Astha] Add: EmployeeWageUC8

--- EmployeeWage/EmployeeWage.cs
+++ EmployeeWage/EmployeeWage.cs
@@ -8,9 +8,8 @@ namespace EmployeeWage
 {
     internal class EmployeeWage
     {
-        public int ComputeWage()
+        public int ComputeWage(int wagePerHour,int noOfWorkingDays,int WorkingHoursPerMonth)
         {
-            const int WAGE_PER_HOUR = 20;
             const int FULL_DAY_HOUR = 8;
             const int PART_TIME_HOUR = 4;
             const int PRESENT = 1;
@@ -20,19 +19,19 @@ namespace EmployeeWage
             Random random = new Random();
             int totalWage = 0;
             int totalWorkingHours = 0;
-            while (day < 20 && totalWorkingHours < 100)
+            while (totalWorkingHours < WorkingHoursPerMonth)
             {
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
                 {
                     case PARTIAL_PRESENT:
-                        totalWage += PART_TIME_HOUR * WAGE_PER_HOUR;
+                        totalWage += PART_TIME_HOUR * wagePerHour;
                         totalWorkingHours += PART_TIME_HOUR;
                         break;
                     case ABSENT:
                         break;
                     case PRESENT:
-                        totalWage += WAGE_PER_HOUR * FULL_DAY_HOUR;
+                        totalWage += wagePerHour * FULL_DAY_HOUR;
                         totalWorkingHours += FULL_DAY_HOUR;
                         break;
                 }
--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -11,8 +11,13 @@ namespace EmployeeWage
         static void Main(string[] args)
         {
             EmployeeWage employeeWage = new EmployeeWage();
-            int totalWage=employeeWage.ComputeWage();
-            Console.WriteLine("Daily employee wage is " + totalWage) ;
+            int totalWageCompany1=employeeWage.ComputeWage(20,30,200);
+            Console.WriteLine("Daily employee wage is for company 1" + totalWageCompany1) ;
+            int totalWageCompany2 = employeeWage.ComputeWage(15, 28, 180);
+            Console.WriteLine("Daily employee wage is for company 2" + totalWageCompany2);
+            int totalWageCompany3 = employeeWage.ComputeWage(25, 30, 250);
+            Console.WriteLine("Daily employee wage is for company 3" + totalWageCompany3);
+
         }
     }
 }
